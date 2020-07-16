ae15c5d00fdb271babb73051d13dd7f20d0b31e1
hinsxd <hins.xd@hotmail.com>
2020-07-16 23:13:48 +0800

    DB

--- ormconfig.js
+++ ormconfig.js
@@ -2,8 +2,8 @@ module.exports = {
   type: 'postgres',
   host: 'x.hinsxd.dev',
   port: 5432,
-  username: 'demo',
-  password: 'demo',
+  username: 'admin',
+  password: 'password',
   database:
     process.env.NODE_ENV === 'production' ? 'fox-demo-prod' : 'fox-demo-dev',
   synchronize: true,
